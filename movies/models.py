2c514d7c3d7d8820c3a8f75f62e276291b2af39e
MithilCherkadi <mcherkadi@gmail.com>
2025-02-04 21:07:34 -0500

    Database is now linked to whats shown
    Before it was just dummy text but now if you add a movie it actually shows it on the website

--- movies/models.py
+++ movies/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db import models
 class Movie(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, default="")
     price = models.IntegerField()
     description = models.TextField()
     image = models.ImageField(upload_to='movie_images/')
