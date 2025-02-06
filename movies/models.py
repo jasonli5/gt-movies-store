6a3c165dac3675c88e3aa2c0d3cd23dbe375a538
dmunagapati <dmunagapati@gmail.com>
2025-02-06 14:30:38 -0500

    Fixed errors and now successfully detecting Movie and Review for migrations

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    release_date = models.DateField()
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    content = models.TextField()
+    rating = models.IntegerField()
