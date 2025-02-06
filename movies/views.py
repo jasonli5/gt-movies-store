cb4034727e5370d3a0884cc8ef6c17bf3a1fc0d1
MithilCherkadi <mcherkadi@gmail.com>
2025-02-06 14:02:34 -0500

    Search Function
    Added a search function to the movies page

--- movies/views.py
+++ movies/views.py
@@ -23,8 +23,13 @@ movies = [
 
 def index(request):
     template_data = {}
+    search_term = request.GET.get('search')
+    if search_term:
+        movies =Movie.objects.filter(name__icontains=search_term)
+    else:
+        movies = Movie.objects.all()
     template_data['title'] = 'Movies'
-    template_data['movies'] = Movie.objects.all()
+    template_data['movies'] = movies
     return render(request, 'movies/index.html',
                   {'template_data': template_data})
 
