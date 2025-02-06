7881f786c25e35bb0e44445183d6130957da227f
Neha Kunche <nehakunche@lawn-128-61-64-141.lawn.gatech.edu>
2025-02-06 12:02:09 -0500

    Dummy Data Movie Page

--- movies/views.py
+++ movies/views.py
@@ -2,6 +2,24 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Movie, Review
 from django.contrib.auth.decorators import login_required
 
+movies = [
+    {
+        'id': 1, 'name': 'Inception', 'price': 12,
+        'description': 'A mind-bending heist thriller.'
+    },
+    {
+        'id': 2, 'name': 'Avatar', 'price': 13,
+        'description': 'A journey to a distant world and the battle for resources.'
+},
+{
+'id': 3, 'name': 'The Dark Knight', 'price': 14,
+'description': 'Gothams vigilante faces the Joker.'
+},
+{
+    'id': 4, 'name': 'Titanic', 'price': 11,
+    'description': 'A love story set against the backdrop of the sinking Titanic.',
+},
+]
 
 def index(request):
     template_data = {}
