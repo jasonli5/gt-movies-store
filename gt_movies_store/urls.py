2db421077b6e7678e2fff86b4d70006602972c59
Neha Kunche <nehakunche@lawn-128-61-64-141.lawn.gatech.edu>
2025-02-11 14:05:47 -0500

    added cart feature

--- gt_movies_store/urls.py
+++ gt_movies_store/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('', include('home.urls')),
     path('movies/', include('movies.urls')),
+    path('cart/', include('cart.urls')),
 
 ]
 urlpatterns += static(settings.MEDIA_URL,
--- movies/views.py
+++ movies/views.py
@@ -10,15 +10,15 @@ movies = [
     {
         'id': 2, 'name': 'Avatar', 'price': 13,
         'description': 'A journey to a distant world and the battle for resources.'
-},
-{
-'id': 3, 'name': 'The Dark Knight', 'price': 14,
-'description': 'Gothams vigilante faces the Joker.'
-},
-{
-    'id': 4, 'name': 'Titanic', 'price': 11,
-    'description': 'A love story set against the backdrop of the sinking Titanic.',
-},
+    },
+    {
+        'id': 3, 'name': 'The Dark Knight', 'price': 14,
+        'description': 'Gothams vigilante faces the Joker.'
+    },
+    {
+        'id': 4, 'name': 'Titanic', 'price': 11,
+     'description': 'A love story set against the backdrop of the sinking Titanic.',
+    },
 ]
 
 def index(request):
