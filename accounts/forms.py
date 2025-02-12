efd7e128532b3f404e4341afff76135d09be1055
dmunagapati <dmunagapati@gmail.com>
2025-02-12 16:20:57 -0500

    Fully implemented password reset functionality

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.forms.utils import ErrorList
 from django.utils.safestring import mark_safe
+from django.contrib.auth.models import User
+from django import forms
 
 class CustomErrorList(ErrorList):
     def __str__(self):
@@ -9,13 +11,21 @@ class CustomErrorList(ErrorList):
         return mark_safe(''.join([
             f'<div class="alert alert-danger" role="alert">{e}</div>' for e in self]))
 
-
 class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(
+        required=True,
+        help_text="Required. Enter a valid email address.",
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
+
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-        for fieldname in ['username', 'password1',
-        'password2']:
+        for fieldname in ['username', 'password1', 'password2', 'email']:
             self.fields[fieldname].help_text = None
             self.fields[fieldname].widget.attrs.update(
                 {'class': 'form-control'}
             )
+
+    class Meta: 
+        model = User
+        fields = ["username", "email", "password1", "password2"]
