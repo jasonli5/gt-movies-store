67c0afa24fd758ee8f81c1d530f85338b0e25810
jasonli5 <jasonli2068@gmail.com>
2025-02-12 19:57:12 -0500

    add orders and purchases page

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,13 +11,13 @@ urlpatterns = [
      path('password-reset/done/',  
           CustomPasswordResetDoneView.as_view(),  
           name='password_reset_done'),
-
      path(
         'password-reset-confirm/<uidb64>/<token>/',
         CustomPasswordResetConfirmView.as_view(),
         name='password_reset_confirm'
-    ),
-    path('password-reset-complete/',
+     ),
+     path('password-reset-complete/',
          auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
          name='password_reset_complete'),
+     path('orders/', views.orders, name='accounts.orders'),
 ]
