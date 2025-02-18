70982e8749bcc5e85d6dcd716ef4c1488703bd19
MithilCherkadi <mcherkadi@gmail.com>
2025-02-18 04:32:41 -0500

    slight changes to ordering of the parameters
    changed the order of email and username

--- accounts/forms.py
+++ accounts/forms.py
@@ -30,6 +30,7 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta: 
         model = CustomUser
         fields = ["username", "email", "password1", "password2"]
+
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
