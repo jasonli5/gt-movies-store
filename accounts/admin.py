2c73f30c52bc3b0f8e1b88adebea2ce7cae7534a
MithilCherkadi <mcherkadi@gmail.com>
2025-02-17 23:07:31 -0500

    Email added to admin user
    Email field is added to the user admin place so when you want to add a user from admin you can put email

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
